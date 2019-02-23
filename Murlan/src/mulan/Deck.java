55c87a1bbb117d79b3ffeec9d785e2ee6cdeccc5
simeonlico <32145586+simeonlico@users.noreply.github.com>
2019-02-23 11:36:27 -0600

    Murlan

---
+++ Murlan/src/mulan/Deck.java
@@ -0,0 +1,37 @@
+package mulan;
+
+import java.util.*;
+
+public class Deck {
+
+	private final int NUMBER_PER_SUIT = 13;
+	private ArrayList<Card> deck;
+	
+	public Deck() {
+		setDeck(new ArrayList<Card>());
+	}
+
+	public ArrayList<Card> getDeck() {
+		return deck;
+	}
+
+	public void setDeck(ArrayList<Card> deck) {
+		this.deck = deck;
+	}
+	
+	public void generateDeck() {
+		for(int i = 3; i < NUMBER_PER_SUIT+3; i++ ) {
+			deck.add(new Card(i, "Hearts"));
+			deck.add(new Card(i, "Diamonds"));
+			deck.add(new Card(i, "Spades"));
+			deck.add(new Card(i, "Clubs"));
+		}
+		deck.add(new Card(17, "Black"));
+		deck.add(new Card(18, "Red"));
+	}
+	
+	public void clearDeck() {
+		deck.clear();
+	}
+		
+}
---
+++ Murlan/src/mulan/Game.java
@@ -0,0 +1,20 @@
+package mulan;
+
+public class Game {
+
+	public Game() {
+		deck = new Deck();
+		deck.generateDeck();
+	}
+	private Deck deck;
+	
+	public void dealCards() {
+		
+	};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
